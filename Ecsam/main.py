9b9213087924ab9c2cbc0e50629a5f7102a1b27e
Marco Carega <marcocarega.dev@gmail.com>
2023-12-24 15:53:08 +0100

    implementation of a pipeline to generate stable models for each type of exercise

--- Ecsam/main.py
+++ Ecsam/main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
+from test_maker import test_maker
 
-# Press Maiusc+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+def main():
+    maker=test_maker()
+    dict={"open" : 5, "multiple" : 5}
+    models=maker.generate(1,dict)
+    print(models)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- Ecsam/test_maker.py
+++ Ecsam/test_maker.py
@@ -4,22 +4,38 @@ import django
 import pylatex
 import pypdf
 
+def to_asp_format(model):
+    string=""
+    string=".".join(str(symbol) for symbol in model.split(" "))
+    if(string!=""):
+        string+="."
+    return string
+
 class test_maker:
 
-    def __init__(self,num_of_exe=10,exe_list=[]):
-        self.num_of_exe=num_of_exe
-        self.exe_list=exe_list
+    def __init__(self):
+        pass
 
-    def generate(self,num_tests,couple_num_exe):
-        models=[]
-        file=open("logic_program.lp","r")
-        program=file.read()
-        args=["--model="+str(num_tests)]
-        control=clingo.Control(arguments=args)
-        control.add("base",[],program)
-        control.ground([("base",[])])
-        handle=control.solve(yield_=True)
-        for model in handle:
-            models+=[model]
-        return models
+    def generate(self,num_tests=1,couple_num_exe={}):
+        tot_exe=0
+        for key in couple_num_exe.keys():
+            tot_exe+=couple_num_exe[key]
+        model=""
+        for key in couple_num_exe.keys():
 
+            num_exe_per_test=couple_num_exe[key]
+
+            file = open("logic_program.lp", "r")
+            program = model+"\n"+file.read()
+            args = ["--model=" + str(num_tests),
+                    "-c num_exe=" + str(num_exe_per_test),
+                    "-c tot_exe=" + str(tot_exe),
+                    "-c type="+key,
+                    ]
+            control = clingo.Control(arguments=args)
+            control.add("base", [], program)
+            control.ground([("base", [])])
+            handle = control.solve(yield_=True)
+            for submodel in handle:
+                model = to_asp_format(str(submodel))
+        return model
